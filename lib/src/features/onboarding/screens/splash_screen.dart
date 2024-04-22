8771d01de0fb68e36fec6bdbcf7dc24eb8ce2b5d
adtoba <adetoba53@gmail.com>
2024-04-22 12:45:06 +0100

    feat: Added logo to splash screen

--- lib/src/features/onboarding/screens/splash_screen.dart
+++ lib/src/features/onboarding/screens/splash_screen.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 
 import 'package:earnwise/src/features/auth/screens/login_screen.dart';
+import 'package:earnwise/src/styles/text_sizes.dart';
 import 'package:earnwise/src/utils/navigator.dart';
+import 'package:earnwise/src/utils/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
@@ -14,8 +17,16 @@ class SplashScreen extends ConsumerStatefulWidget {
 
 class _SplashScreenState extends ConsumerState<SplashScreen> {
 
+  late bool _visible = false;
+
   @override
   void initState() {
+    Timer(const Duration(milliseconds: 500), () {
+      setState(() {
+        _visible = true;
+      });
+    });
+
     Timer(const Duration(seconds: 5), () {
       push(const LoginScreen());
     });
@@ -24,6 +35,22 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: const Duration(seconds: 2),
+          child: Text(
+            "EarnWise",
+            style: Theme.of(context).textTheme.displayLarge?.copyWith(
+              fontWeight: FontWeight.bold,
+              fontSize: config.sp(30),
+              fontFamily: GoogleFonts.raleway().fontFamily
+            ),
+          )
+        ),
+      ),
+    );
   }
 }
